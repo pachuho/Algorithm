e29a6b2863d8b4c243d90d6dc6563a7ce8772b9d
JH <juhopark@beuagjuhoui-MacBook-Air.local>
2022-01-03 23:41:50 +0900

    2588번 (수정필요)

--- main.kt
+++ main.kt
@@ -2,8 +2,29 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.StringTokenizer
 
+var inputNumber = mutableListOf<String>()
+var resultNumber = mutableListOf<Int>()
+var total = 0
+
 fun main(){
-  val br = BufferedReader(InputStreamReader(System.`in`))
-  val token = StringTokenizer(br.readLine())
-  println(token.nextToken().toInt() + token.nextToken().toInt())
+  // 숫자 두번 입력받기
+  for(i in 1..2){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val token = StringTokenizer(br.readLine())
+    inputNumber.add(token.nextToken())
+  }
+
+  for(number in inputNumber[1]){
+    val result = inputNumber[0].toInt() * Character.getNumericValue(number)
+    resultNumber.add(result)
+  }
+
+  var count = 1
+  for(result in resultNumber.reversed()){
+    println(result)
+
+    total += (result * count)
+    count *= 10
+  }
+  println(total)
 }
