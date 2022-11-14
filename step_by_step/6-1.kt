9a4f14a547f2f22446ed37a556cacc14b86e7f79
pachuho <juho7410@gmail.com>
2022-11-14 19:31:08 +0900

    [Create] step 6-1

---
+++ step_by_step/6-1.kt
@@ -0,0 +1,12 @@
+package step_by_step
+
+import java.util.*
+
+fun main(){
+    val br = System.`in`.bufferedReader()
+    val char = StringTokenizer(br.readLine()).nextToken()[0]
+
+    println(char.code)
+}
+
+// https://www.acmicpc.net/problem/11654
