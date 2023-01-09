4a558c98d3dfd5d9cc5892e9f5fb8e2d19308ca6
pachuho <juho7410@gmail.com>
2023-01-09 16:22:57 +0900

    add baekjoon 1966

--- silver/1966.kt
+++ silver/1966.kt
@@ -3,13 +3,13 @@ package silver
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
-import kotlin.contracts.contract
 
 // 프린터 큐
 fun main(){
     val br = BufferedReader(InputStreamReader(System.`in`))
     val loop = br.readLine().toInt()
     val queue = ArrayDeque<Pair<Int, Int>>() // 순서, 중요도
+    val resultQueue = ArrayDeque<Pair<Int, Int>>() // 결과
     var token: StringTokenizer
     var n: Int // 문서 개수
     var m: Int // 몇 번째로 출력되는지 궁금한 문서 위치
@@ -24,25 +24,26 @@ fun main(){
             queue.offer(Pair(it, token.nextToken().toInt())) // 큐에 대기열 추가
         }
 
-//        println(queue.toString())
-        // 1 2 3 4
         // 우선순위 비교
-        var condition = queue.size
-        while (queue.size > 1 && condition > 0){
-            var pass = false
-            for (i in queue.indices){
+        while (queue.size > 1){
+            var pass = true
+            for(i in queue.indices){
                 if(i == queue.size - 1) break
-                if(queue.elementAt(i).second < queue.elementAt(i + 1).second){
-                    queue.offer(queue.pop())
-                } else{
-                    pass = true
+
+                if(queue.elementAt(0).second < queue.elementAt(i + 1).second){
+                    queue.add(queue.pop())
+                    pass = false
+                    break
                 }
             }
-            if(pass) condition --
+            if(pass) resultQueue.add(queue.pop())
         }
-        println(queue.toString())
 
-        queue.clear()
+        resultQueue.add(queue.pop())
+
+        for(i in resultQueue.indices){
+            if( resultQueue.elementAt(i).first == m) println(i+1)
+        }
+        resultQueue.clear()
     }
-
 }
