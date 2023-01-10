2983669075314f8b665b2e05b8da094a250248e3
pachuho <juho7410@gmail.com>
2023-01-11 00:42:57 +0900

    add baekjoon 색종이

--- silver/색종이.kt
+++ silver/색종이.kt
@@ -2,33 +2,21 @@ package silver
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.math.abs
 
 // 2563
 fun main(){
     val br = BufferedReader(InputStreamReader(System.`in`))
-    val num = br.readLine().toInt()
-    val papers = ArrayList<Margin>()
-    var overLapping = 0
+    val field = Array(100){ BooleanArray(100) }
 
-    repeat(num){
-        val token = StringTokenizer(br.readLine())
-        papers.add(Margin(token.nextToken().toInt(), token.nextToken().toInt()))
-    }
+    repeat(br.readLine().toInt()){
+        val (p1, p2) = br.readLine().split(" ").map { it.toInt() }
 
-    for(me in papers.indices){
-        for(target in papers.indices){
-            if(me >= target) continue
-            val diffLeft = abs(papers[me].marginLeft - papers[target].marginLeft)
-            val diffBottom = abs(papers[me].marginBottom - papers[target].marginBottom)
-
-            if( diffLeft >= 10 || diffBottom >= 10) continue
-            overLapping += (10 - diffLeft) * (10 - diffBottom)
+        repeat(10){ dx ->
+            repeat(10){ dy ->
+                field[p1 + dx][p2 + dy] = true
+            }
         }
     }
-    println((num * 10 * 10) - overLapping)
 
+    println(field.sumOf { it.count { it } })
 }
-data class Margin(val marginLeft: Int, val marginBottom: Int)
